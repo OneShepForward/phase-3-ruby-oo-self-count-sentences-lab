ce0fb645c25230115ff58a1ca55199b5aa55e364
Nicholas Shephard <shephardn@gmail.com>
2021-12-16 08:37:43 -0600

    commit tree 51140ac9c0e97b528aa36f67dd91d1de7f3e20c2

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    # self =~ /[.!?]/
+    new_array = self.split(/[.!?]/)
+    newer_array = new_array.reject {|item| item == ""}.length
+    # sentence_number = new_array.length
+    # binding.pry
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...
+".count_sentences
